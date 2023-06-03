a36d50767ee71bd6c11a8c885a18e15b5587ebac
Michael G. Noll <miguno@miguno.com>
2023-06-03 11:47:31 +0200

    Refactor

--- src/main.c
+++ src/main.c
@@ -2,10 +2,18 @@
 #include <stdio.h>
 
 int main() {
-  cl_connect();
+  // Connect to chess board device with hid.  If the device is not connected,
+  // it will automatically connect when the device is plugged into the computer.
+  printf("[DEBUG] Connecting to chess board via HID...\n");
+  int success = cl_connect();
+  if (success) {
+    printf("Successfully connected to chess board\n");
+  } else {
+    printf("ERROR: Failed to connect to chess board\n");
+  }
 
-  char version[10];
   int num_chars;
+  char version[10];
   num_chars = cl_version(version);
   printf("SDK version: %.*s\n", num_chars, version);
 
@@ -16,4 +24,9 @@ int main() {
   char ble_version[100];
   num_chars = cl_get_ble_version(ble_version);
   printf("BLE hardware version: %.*s\n", num_chars, ble_version);
+
+  printf("Battery level: %d%%\n", cl_get_battery());
+
+  printf("[DEBUG] Disconnecting from chessboard\n");
+  cl_disconnect();
 }
