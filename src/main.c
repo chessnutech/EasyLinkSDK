d34f99b0a98f0bda60c9df4f51f88312683d99c7
Michael G. Noll <michael@michael-noll.com>
2023-09-21 21:38:11 +0200

    Improve example code

--- src/main.c
+++ src/main.c
@@ -8,8 +8,8 @@
 
 int main(void) {
   char sdk_version[RECOMMENDED_SDK_VERSION_LENGTH];
-  int sdk_version_length = cl_version(sdk_version);
-  printf("[DEBUG] SDK version: %.*s\n", sdk_version_length, sdk_version);
+  const size_t sdk_version_length = cl_version(sdk_version);
+  printf("[DEBUG] SDK version: %.*s\n", (int)sdk_version_length, sdk_version);
 
   // Connect to chessboard device with HID.  If the device is not connected,
   // it will automatically connect when the device is plugged into the computer.
@@ -23,20 +23,20 @@ int main(void) {
 
   // MCU
   char mcu_version[RECOMMENDED_MCU_VERSION_LENGTH];
-  int mcu_version_length = cl_get_mcu_version(mcu_version);
+  const size_t mcu_version_length = cl_get_mcu_version(mcu_version);
   if (mcu_version_length > 0) {
-    printf("MCU hardware version: %.*s\n", mcu_version_length, mcu_version);
+    printf("MCU hardware version: %.*s\n", (int)mcu_version_length, mcu_version);
   }
 
   // BLE (Bluetooth Low Energy)
   char ble_version[RECOMMENDED_BLE_VERSION_LENGTH];
-  int ble_version_length = cl_get_ble_version(ble_version);
+  const size_t ble_version_length = cl_get_ble_version(ble_version);
   if (ble_version_length > 0) {
-    printf("BLE hardware version: %.*s\n", ble_version_length, ble_version);
+    printf("BLE hardware version: %.*s\n", (int)ble_version_length, ble_version);
   }
 
   // Battery
-  int battery_level = cl_get_battery();
+  const int battery_level = cl_get_battery();
   if (battery_level >= 0) {
     printf("Battery level: %d%%\n", battery_level);
   }
@@ -47,6 +47,14 @@ int main(void) {
     printf("[ERROR] Could not enable/disable LEDs\n");
   }
 
+  const int file_count = cl_get_file_count();
+  if (file_count >= 0) {
+    printf("Stored game files: %d\n", file_count);
+  } else {
+    printf("[ERROR] Could not retrieve number of stored game files");
+  }
+
+  cl_disconnect();
   printf("[DEBUG] Disconnecting from chessboard\n");
   cl_disconnect();
   return EXIT_SUCCESS;
