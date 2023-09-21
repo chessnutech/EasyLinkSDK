6b3f5c301a8c40f246f94e764a4f48d698a07c4e
Michael G. Noll <michael@michael-noll.com>
2023-09-21 21:41:01 +0200

    Use enum instead of macros

--- src/main.c
+++ src/main.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define RECOMMENDED_SDK_VERSION_LENGTH 20
-#define RECOMMENDED_MCU_VERSION_LENGTH 100
-#define RECOMMENDED_BLE_VERSION_LENGTH 100
+enum {
+  recommended_sdk_version_length = 20,
+  recommended_mcu_version_length = 100,
+  recommended_ble_version_length = 100
+};
 
 int main(void) {
-  char sdk_version[RECOMMENDED_SDK_VERSION_LENGTH];
+  char sdk_version[recommended_sdk_version_length];
   const size_t sdk_version_length = cl_version(sdk_version);
   printf("[DEBUG] SDK version: %.*s\n", (int)sdk_version_length, sdk_version);
 
@@ -22,14 +24,14 @@ int main(void) {
   }
 
   // MCU
-  char mcu_version[RECOMMENDED_MCU_VERSION_LENGTH];
+  char mcu_version[recommended_mcu_version_length];
   const size_t mcu_version_length = cl_get_mcu_version(mcu_version);
   if (mcu_version_length > 0) {
     printf("MCU hardware version: %.*s\n", (int)mcu_version_length, mcu_version);
   }
 
   // BLE (Bluetooth Low Energy)
-  char ble_version[RECOMMENDED_BLE_VERSION_LENGTH];
+  char ble_version[recommended_ble_version_length];
   const size_t ble_version_length = cl_get_ble_version(ble_version);
   if (ble_version_length > 0) {
     printf("BLE hardware version: %.*s\n", (int)ble_version_length, ble_version);
