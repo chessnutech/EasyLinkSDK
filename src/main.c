d0dee9a13fac4f09cfe17d0930ac8f7cd72a8d2d
Michael G. Noll <miguno@miguno.com>
2023-09-22 19:56:13 +0200

    Use clang-tidy suggested enum naming

--- src/main.c
+++ src/main.c
@@ -2,14 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-enum {
-  recommended_sdk_version_length = 20,
-  recommended_mcu_version_length = 100,
-  recommended_ble_version_length = 100
-};
+enum { kRecommendedSdkVersionLength = 20, kRecommendedMcuVersionLength = 100, kRecommendedBleVersionLength = 100 };
 
 int main(void) {
-  char sdk_version[recommended_sdk_version_length];
+  char sdk_version[kRecommendedSdkVersionLength];
   const size_t sdk_version_length = cl_version(sdk_version);
   if (sdk_version_length > 0) {
     printf("[DEBUG] SDK version: %.*s\n", (int)sdk_version_length, sdk_version);
@@ -26,14 +22,15 @@ int main(void) {
   }
 
   // MCU
-  char mcu_version[recommended_mcu_version_length];
+  char mcu_version[kRecommendedMcuVersionLength];
   const size_t mcu_version_length = cl_get_mcu_version(mcu_version);
   if (mcu_version_length > 0) {
     printf("MCU hardware version: %.*s\n", (int)mcu_version_length, mcu_version);
   }
 
   // BLE (Bluetooth Low Energy)
-  char ble_version[recommended_ble_version_length];
+  char ble_version[kRecommendedBleVersionLength];
+
   const size_t ble_version_length = cl_get_ble_version(ble_version);
   if (ble_version_length > 0) {
     printf("BLE hardware version: %.*s\n", (int)ble_version_length, ble_version);
@@ -48,7 +45,7 @@ int main(void) {
   printf("Enabling LEDs for squares d5 and e4\n");
   const char *leds[8] = {"00000000", "00000000", "00000000", "00010000",
                          "00001000", "00000000", "00000000", "00000000"};
-  
+
   if (cl_led(leds) == 0) {
     printf("[ERROR] Could not enable/disable LEDs\n");
   }
